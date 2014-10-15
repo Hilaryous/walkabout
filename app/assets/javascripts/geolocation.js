c9b7b9b3696d55a98b51525408bc6a60506536e1
Hilary Denton <Hilarydenton90@gmail.com>
2014-10-15 14:23:59 -0600

    move location finder

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,8 +1,10 @@
-function getGeoLocation() {
-  navigator.geolocation.getCurrentPosition(setGeoCookie);
+function get_location() {
+  navigator.geolocation.getCurrentPosition(setLocations);
 }
 
-function setGeoCookie(position) {
+function setLocations(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
   var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
   document.cookie = "lat_lng=" + escape(cookie_val);
 }
